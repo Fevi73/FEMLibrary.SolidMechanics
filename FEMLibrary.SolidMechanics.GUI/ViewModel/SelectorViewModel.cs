c652d44a2af77f58af7acebd7d3dbb9175702e76
Taras <taras.horyachko@gmail.com>
2014-05-06 18:46:00 +0300

    add selector view model1

--- FEMLibrary.SolidMechanics.GUI/ViewModel/SelectorViewModel.cs
+++ FEMLibrary.SolidMechanics.GUI/ViewModel/SelectorViewModel.cs
@@ -9,16 +9,24 @@ namespace FEMLibrary.SolidMechanics.GUI.ViewModel
 {
     public class SelectorViewModel : ViewModelBase
     {
+        private void showMainWindow(SetupViewModel viewModel)
+
         public RelayCommand ShowCylindricalPlateSolverCommand { get; private set; }
 
         public void ShowCylindricalPlateSolver()
         {
+            MainWindow window = new MainWindow();
+            window.DataContext = new CylindricalPlateSetupViewModel();
+            window.Show();
         }
 
         public RelayCommand ShowRectanglePlateSolverCommand { get; private set; }
 
         public void ShowRectanglePlateSolver()
         {
+            MainWindow window = new MainWindow();
+            window.DataContext = new RectangleSetupViewModel();
+            window.Show();
         }
     }
 }
